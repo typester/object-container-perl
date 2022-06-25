6f1eb0f655c26b52c94167fa8c25a988c06c3e74
bayashi <bayashi@cpan.org>
2022-06-25 06:04:52 +0000

    bump version 0.15

--- lib/Object/Container.pm
+++ lib/Object/Container.pm
@@ -5,7 +5,7 @@ use warnings;
 use parent qw(Class::Accessor::Fast);
 use Carp;
 
-our $VERSION = '0.14';
+our $VERSION = '0.15';
 
 __PACKAGE__->mk_accessors(qw/registered_classes autoloader_rules objects/);
 
